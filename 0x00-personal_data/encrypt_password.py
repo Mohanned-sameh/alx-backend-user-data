b8596ede96abb70d8d14ee5e619e3dbd868b888c
Mohanned-sameh <mohanned2678@gmail.com>
2024-05-30 14:18:05 +0300

    feat: Add function to check if a password matches the hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Returns a salted, hashed password, which is a byte string."""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Returns a boolean indicating whether or not the provided password
+    matches the hashed password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
