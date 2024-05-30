b791ed5ddfd4127c8fbb8e44956dd2067e7007dd
Mohanned-sameh <mohanned2678@gmail.com>
2024-05-30 13:57:32 +0300

    Refactor RedactingFormatter class for improved readability and maintainability

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,31 @@ function that returns log message obfuscated
 """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str] = [], redaction: str = REDACTION):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+        self.redaction = redaction
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record
+        """
+        return filter_datum(
+            self.fields,
+            self.redaction,
+            super(RedactingFormatter, self).format(record),
+            self.SEPARATOR,
+        )
 
 
 def filter_datum(
