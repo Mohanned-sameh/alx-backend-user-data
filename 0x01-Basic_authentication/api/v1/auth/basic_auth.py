7f197b08dbac3eae9daaa495a99a73dac0ed2951
Mohanned-sameh <mohanned2678@gmail.com>
2024-06-03 19:25:48 +0300

    Refactor extract_user_credentials method in BasicAuth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,3 +40,17 @@ class BasicAuth(Auth):
             return None
 
         return decoded
+
+    def extract_user_credentials(
+        self,
+        decoded_base64_authorization_header: str,
+    ) -> (str, str):  # type: ignore
+        """Extracts user credentials"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        user_pass = decoded_base64_authorization_header.split(":", 1)
+        return (user_pass[0], user_pass[1])
