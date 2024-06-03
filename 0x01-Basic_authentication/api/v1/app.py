9863b3c9f51a0fb24024cf76d8c7397529700ce1
Mohanned-sameh <mohanned2678@gmail.com>
2024-06-03 19:17:57 +0300

    chore: Refactor authentication logic to use Auth class and add unauthorized and forbidden routes

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,13 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+auth = None
+if os.getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+
+    auth = Auth()
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """Not found handler"""
@@ -30,6 +37,19 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request() -> None:
+    """Before request"""
+    if auth is None:
+        return
+    excluded_paths = ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+    if auth.require_auth(request.path, excluded_paths):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
 
 
 if __name__ == "__main__":
