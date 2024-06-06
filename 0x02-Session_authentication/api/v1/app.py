f1e37de25f2689a1f002197aa236dd90c68fddd7
Mohanned-sameh <mohanned2678@gmail.com>
2024-06-07 02:25:33 +0300

    Refactor authentication logic to use SessionAuth class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS, cross_origin
 import os
-
+from api.v1.auth.session_auth import SessionAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -24,6 +24,9 @@ if os.getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
 
     auth = BasicAuth()
+
+if os.getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
