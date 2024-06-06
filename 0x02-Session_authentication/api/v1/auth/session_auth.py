947a7247065018cee5d30c6b6f0ed7589ad77a11
Mohanned-sameh <mohanned2678@gmail.com>
2024-06-07 02:30:31 +0300

    Refactor create_session method in SessionAuth class to generate a Session ID using uuid4

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,38 +1,19 @@
 #!/usr/bin/env python3
 """Session Authentication"""
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
-    """Session Authentication Class"""
+    """Session Authentication"""
 
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Create a Session ID"""
-        if user_id is None or type(user_id) is not str:
+        """Create a Session ID for a user_id"""
+        if user_id is None or user_id is not type(str):
             return None
-        session_id = super().create_session(user_id)
-        if session_id is None:
-            return None
-        self.user_id_by_session_id[session_id] = user_id
-        return session_id
-
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Return a User ID based on a Session ID"""
-        if session_id is None or type(session_id) is not str:
-            return None
-        return self.user_id_by_session_id.get(session_id)
-
-    def destroy_session(self, request=None) -> bool:
-        """Destroy a session ID"""
-        if request is None:
-            return False
-        session_id = self.session_cookie(request)
-        if session_id is None:
-            return False
-        user_id = self.user_id_for_session_id(session_id)
-        if not user_id:
-            return False
-        del self.user_id_by_session_id[session_id]
-        return True
+        else:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
