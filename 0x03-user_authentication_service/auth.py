25309f18f5030750946290be6cd6a8e8ef3052d5
mohanned-sameh <mohanned2678@gmail.com>
2024-06-14 16:41:12 +0300

    chore: Refactor auth module to include user registration functionality

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,9 +2,27 @@
 """
 auth file
 """
+from db import DB
 import bcrypt
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     """Hash a password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth Class"""
+
+    def __init__(self):
+        """Constructor"""
+        self.db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user"""
+        try:
+            self.db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except Exception:
+            return self.db.add_user(email, _hash_password(password))
