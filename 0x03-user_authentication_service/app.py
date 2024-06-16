b6622ebd578a164e2f4f1fe31d279330034b4ca0
mohanned-sameh <mohanned2678@gmail.com>
2024-06-16 17:09:17 +0300

    Refactor user registration endpoint to handle existing email check

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -20,5 +20,19 @@ def home() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """
+    users endpoint
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
