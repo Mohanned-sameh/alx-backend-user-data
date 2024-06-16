46940112b561f53e64b60a85a87a9a905f071d8e
mohanned-sameh <mohanned2678@gmail.com>
2024-06-16 17:11:46 +0300

    done

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -34,5 +34,48 @@ def users() -> str:
         return jsonify({"message": "email already registered"})
 
 
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """
+    login endpoint
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """
+    logout endpoint
+    """
+    session_id = request.cookies.get("session_id")
+    email = AUTH.get_user_from_session_id(session_id)
+    if email:
+        AUTH.destroy_session(email)
+        return jsonify({"message": "Bienvenue"})
+    else:
+        abort(403)
+
+
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> str:
+    """
+    profile endpoint
+    """
+    session_id = request.cookies.get("session_id")
+    email = AUTH.get_user_from_session_id(session_id)
+    if email:
+        return jsonify({"email": email})
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
